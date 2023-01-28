ab609fbc0787ac6f294e6d9ca36eeba9817f7348
juli1994 <julihartawan06@gmail.com>
2023-01-28 15:59:27 +0700

    pembayaran

--- app/Controllers/Report.php
+++ app/Controllers/Report.php
@@ -10,6 +10,7 @@ class Report extends BaseController
 
     public function __construct()
     {
+        $this->db = db_connect();
 		$this->validation =  \Config\Services::validation();
         $this->session = \Config\Services::session();
         $this->toko = new TokoModel();
@@ -38,12 +39,37 @@ class Report extends BaseController
         }
     }
 
+
+    public function getData($awal, $akhir){
+        $sql = 'SELECT invoice,nm_user," " qty," " harga_buku,sub_total AS total,tgl_order  from tbl_order tor 
+        INNER JOIN tbl_user_biodata tub ON tub.id_user_bio = tor.id_user_bio 
+        WHERE 
+        tgl_order >= "'.$awal.'" AND tgl_order <= "'.$akhir.'"
+         
+         UNION all
+         
+         SELECT tor.invoice,judul_buku,qty,harga_buku," " sub_total," " tgl_order FROM tbl_order_detail tod 
+         INNER JOIN tbl_order tor ON tor.id_order = tod.id_order
+         left JOIN tbl_product tp ON tp.id_product = tod.id_product 
+         inner JOIN tbl_toko tt ON tp.id_toko = tt.id_toko WHERE tor.tgl_order >= "'.$awal.'" AND tor.tgl_order <= "'.$akhir.'" 
+         ORDER BY invoice, tgl_order DESC ';
+
+         return $this->db->query($sql)->getResult();
+    }
+
+
     public function cetak()
     {
         
         $fields['awal'] = $this->request->getPost('date1');
         $fields['akhir'] = $this->request->getPost('date2');
         $fields['toko'] = $this->session->get('username') == 'admin' ? $this->request->getPost('toko') : $this->session->get('id_user_bio');
+        //data penjualan
+        $fields['data'] = $this->getData($fields['awal'],$fields['akhir']);
+
+        // var_dump($this->getData($fields['awal'],$fields['akhir']));
+        // var_dump($fields);
+        // die;
 
         $this->validation->setRules([
             'awal' => ['label'=>'awal','rules'=>'required','errors' => ['required' => 'Harap memasukan tanggal dengan benar']],
@@ -52,7 +78,7 @@ class Report extends BaseController
 
         // var_dump($this->validation->getErrors());die;
         if ($this->validation->run($fields) == FALSE) {
-            var_dump($this->validation->getErrors());die;
+            // var_dump($this->validation->getErrors());die;
             $data = [
                 'controller'        => 'Laporan',
                 'title'             => 'Laporan',
@@ -69,7 +95,8 @@ class Report extends BaseController
         return view('cetak',[
             'awal' => $fields['awal'],
             'akhir' => $fields['akhir'],
-            'toko' => $toko[0]->nm_toko
+            'toko' => $toko[0]->nm_toko,
+            'data' => $fields['data']
             ]);
 
             // print_r($html);
