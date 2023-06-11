ddb5f19c1b38e672555fbbdc33e4cd695f1b837b
juli1994 <julihartawan06@gmail.com>
2023-06-11 19:02:53 +0700

    revisi

--- app/Controllers/Toko.php
+++ app/Controllers/Toko.php
@@ -28,7 +28,7 @@ class Toko extends BaseController
 		if (session()->get('isLogin')) {
 
 			if (session()->get('username') == 'admin') {
-				$userbio = $this->user->select('*')->join('tbl_user_biodata', 'tbl_user.username = tbl_user_biodata.nik_user')->where('tbl_user.role', '1')->get();
+				$userbio = $this->user->select('*')->join('tbl_user_biodata', 'tbl_user.nik_user = tbl_user_biodata.nik_user')->where('tbl_user.role', '1')->get();
 			} else {
 				$userbio = $this->user->select('*')->join('tbl_user_biodata', 'tbl_user.nik_user = tbl_user_biodata.nik_user')->where(['tbl_user.role' => '1',
 				'tbl_user.username' => session()->get('username')])->get();
